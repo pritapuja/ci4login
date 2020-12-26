0a96d09b5424ca4f266c76bd802bae66ed54989f
Prita Puja Astuti <prita.pujaastuti@gmail.com>
2020-12-26 08:00:56 +0700

    upload ci4login di github

---
+++ app/Views/templates/sidebar.php
@@ -0,0 +1,103 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-file-alt"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Projek</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Profile
+    </div>
+
+    <!-- Nav Item - My Profile -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#">
+            <i class="fas fa-user"></i>
+            <span>My Profile</span>
+        </a>
+    </li>
+
+    <!-- Nav Item - Edit Profile -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#">
+            <i class="fas fa-user-edit"></i>
+            <span>Edit Profile</span>
+        </a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Interface
+    </div>
+
+    <!-- Nav Item - Use Case Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-male"></i>
+            <span>Use Case</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Custom Use Case:</h6>
+                <a class="collapse-item" href="buttons.html">Aktor</a>
+                <a class="collapse-item" href="cards.html">Case</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Data Flow Structure Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#">
+            <i class="fas fa-tree"></i>
+            <span>Data Flow Structure</span>
+        </a>
+    </li>
+
+    <!-- Nav Item - User Flow Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#">
+            <i class="fas fa-portrait"></i>
+            <span>User Flow</span>
+        </a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Nav Item - Logout -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#">
+            <i class="fas fa-sign-out-alt"></i>
+            <span>Logout</span>
+        </a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
